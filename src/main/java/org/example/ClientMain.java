51fe17082d0537121f87ea0f983b428f35951199
NataliaMuchova <nataliiamuchova@gmail.com>
2025-10-09 18:27:57 +0200

    ClientMain done

--- src/main/java/org/example/ClientMain.java
+++ src/main/java/org/example/ClientMain.java
@@ -2,23 +2,30 @@ package org.example;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import via.pro3.grpc.generated.*;
 
 public class ClientMain {
     public static void main(String[] args)
     {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 3004)
-                .usePlaintext()
-                .build();
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 3004).usePlaintext().build();
 
-        TextConverterGrpc.TextConverterBlockingStub stub =
-              TextConverterGrpc.newBlockingStub(channel);
+        SlaughterHouseGrpc.SlaughterHouseBlockingStub stub = SlaughterHouseGrpc.newBlockingStub(channel);
 
-           RequestText request = RequestText.newBuilder()
-                .setInputText("rpc with streaming based on http/2")
-               .build();
+           AnimalRequest animalRequest = AnimalRequest.newBuilder().setAnimalId(1).build();
 
-        ResponseText response = stub.toUpper(request);
-        System.out.println("Response: " + response.getOutputText());
+      ProductListResponse productResponse = stub.getProductsByAnimal(animalRequest);
+      System.out.println("\nProducts for animal ID 2:");
+      productResponse.getProductsList().forEach(product ->
+          System.out.println(" - " + product.getProductType() + " (ID: " + product.getProductId() + "), trays: " + product.getTrayIdsList())
+      );
+
+      ProductRequest productRequest = ProductRequest.newBuilder().setProductId(1).build();
+
+      AnimalListResponse animalResponse = stub.getAnimalsByProduct(productRequest);
+      System.out.println("\nAnimals for product ID 1:");
+      animalResponse.getAnimalsList().forEach(animal ->
+          System.out.println(" - " + animal.getType() + " (" + animal.getRegistrationNumber() + "), weight: " + animal.getWeight())
+      );
 
         channel.shutdown();
     }
